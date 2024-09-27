b70dc9dc75eb0e19e393d0d13e9d055f808eeca4
unknown <yenigarcia2815@gmail.com>
2024-09-27 11:44:39 -0600

    Agrega express

---
+++ express_server/app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app= express()
+
+app.get('/', function(req,res){
+    res.send('Hello World')
+})
+
+app.get('/loquesea', function(req,res){
+    res.send('lo que sea')
+})
+
+app.get('*', function(req,res){
+    res.send('404 | NO FOUND')
+})
+
+app.listen(3000)
