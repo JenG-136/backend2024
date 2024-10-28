12595d28d81f3a2512f261f1920060af26588a7d
unknown <yenigarcia2815@gmail.com>
2024-10-28 11:16:42 -0600

    Agrega el endpoits de tarea q es agregar users, actualizar y eliminar, y tambien crerar las rutas

--- stock_manager/src/controllers/users.js
+++ stock_manager/src/controllers/users.js
@@ -1,10 +1,9 @@
-const {resquest, response, request} = require('express');
+const { request, response } = require('express');
 
-const users= [//crear un arreglo
-{id: 1, name: 'Jeni GR'}, //rEGISTRO
-{id: 2, name: 'Jen GR'},
-{id: 3, name: 'Lalis ig'},
-
+const users = [ //crear arreglo
+  { id: 1, name: 'Jeni GR' }, //los registro que se va a almacenar
+  { id: 2, name: 'Jen GR' },
+  { id: 3, name: 'Lalis ig' },
 ];
 
 /*const getMessage = (req = resquest, res=response) => { //se va a crear la funcion que responda la consulta en particular
@@ -13,29 +12,85 @@ res.send('Hello Bangtan_ HELLO FROM THE USERS CONTROLLER!');
 
 } */
 
-const getAll = (req = resquest, res=response) => { 
-    res.send(users); //se pide que muestre el usuario
+// paraObtener todos los usuarios
+const getAll = (req = request, res = response) => {
+  res.send(users); //se pide que muestre el usuario
+};
 
-}
+// para Obtener un usuario por ID
+const getById = (req = request, res = response) => {
+  const { id } = req.params; ;//se acceda en el solicitud atreves de req
+  //se tiene que validar un numero por id
 
-const getById = (req = request, res = response) =>{
-    const {id} = req.params;//se acceda en el solicitud atreves de req
-    //se tiene que validar un numero por id
-    if(isNaN(id)){
-        res.status(400).send('Invalid ID');
-        return; 
-    }
+  if (isNaN(id)) {
+    res.status(400).send('Invalid ID');
+    return;
+  }
 
-    //hacer un arrgelo donde pasa un fincion deonde debe terner TRUBUTO Y QUE REPRESENTA EL ARRGELO
-    const user = users.find(user => user.id === +id); 
-    //si el variable de usuario termine el valor si a ningino se debe avisar al users
-    if(!user){
-        res.status(404).send('user not found');
-        return;
-    }
+  //hacer un arrgelo donde pasa un fincion deonde debe terner TRUBUTO Y QUE REPRESENTA EL ARRGELO
+  const user = users.find((user) => user.id === +id);
+  //si el variable de usuario termine el valor si a ningino se debe avisar al users
+  if (!user) {
+    res.status(404).send('User not found');
+    return;
+  }
+  res.send(user);
+};
 
-    res.send(user);
-}
-module.exports = {getAll, getById};
+// paraAgregar un nuevo usuario
+const addUser = (req = request, res = response) => {
+  const { name } = req.body;
+  if (!name) {
+    res.status(400).send('Name is required');
+    return;
+  }
+  const newUser = { id: users.length + 1, name };
+  users.push(newUser);
+  res.status(201).send(newUser);
+};
 
-//para lunes agregar usuario, editar usuario y eliminar y crear las rutas 
+// Actualizar un usuario existente
+const updateUser = (req = request, res = response) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  if (isNaN(id)) {
+    res.status(400).send('Invalid ID');
+    return;
+  }
+
+  const user = users.find((user) => user.id === +id);
+  if (!user) {
+    res.status(404).send('User not found');
+    return;
+  }
+
+  if (!name) {
+    res.status(400).send('Name is required');
+    return;
+  }
+
+  user.name = name;
+  res.send(user);
+};
+
+// Eliminar un usuario
+const deleteUser = (req = request, res = response) => {
+  const { id } = req.params;
+
+  if (isNaN(id)) {
+    res.status(400).send('Invalid ID');
+    return;
+  }
+
+  const index = users.findIndex((user) => user.id === +id);
+  if (index === -1) {
+    res.status(404).send('User not found');
+    return;
+  }
+
+  users.splice(index, 1);
+  res.status(204).send(); // 204 No Content indica éxito sin contenido adicional
+};
+
+module.exports = { getAll, getById, addUser, updateUser, deleteUser };
